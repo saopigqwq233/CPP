45b37d3204e745d6032b3e95b5387c3fee4d32f3
LiuWenhao <115407314+saopigqwq233@users.noreply.github.com>
2023-10-27 17:08:28 +0800

    c++析构、重载

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@ int main()
     Date a(2023,3,15);
     a.print();
     Date b=a;
-    b.print();
+    b+=17;
+    if(b>a)
+    {
+        cout<<'s'<<endl;
+    }
+    cout<<b-a<<endl;
     return 0;
 }
