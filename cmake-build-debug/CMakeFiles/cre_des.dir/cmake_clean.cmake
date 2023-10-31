ad7e20ac621492a198d4b198fc23b6ed3527f797
saopigqwq233 <904139266@qq.com>
2023-10-31 21:35:36 +0800

    c++析构、重载

---
+++ cmake-build-debug/CMakeFiles/cre_des.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cre_des.dir/Date.cpp.obj"
+  "CMakeFiles/cre_des.dir/Date.cpp.obj.d"
+  "CMakeFiles/cre_des.dir/main.cpp.obj"
+  "CMakeFiles/cre_des.dir/main.cpp.obj.d"
+  "cre_des.exe"
+  "cre_des.exe.manifest"
+  "cre_des.pdb"
+  "libcre_des.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cre_des.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
