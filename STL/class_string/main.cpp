86f2f72163aef2faeff7b7e15a087c1c48eb1436
saopigqwq233 <904139266@qq.com>
2024-03-13 13:03:44 +0800

    class_string

---
+++ STL/class_string/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "string"
+using namespace std;
+
+void test1(){
+    string s1("It's not a tough task for you to accomplish it");
+
+}
+
+
+
+
+
+
+
+int main() {
+    test1();
+    return 0;
+}
